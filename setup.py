a20eb51890aaa1f7062ea3c7462ce21f1f8294d5
Xinlun Cheng <chengxinlun@gmail.com>
2019-10-21 14:03:05 -0400

    Satisfy Pypi release requirements

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setuptools.setup(
     version="0.1.0",
     author="Xinlun Cheng",
     author_email="chengxinlun@gmail.com",
+    license='MIT',
     description="Many utility functions for astronomical research",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -17,6 +18,11 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "astropy>=3.0",
+        "numpy>=1.14.0",
+        "matplotlib>=3.0",
     ],
     python_requires='>=3.0',
 )
